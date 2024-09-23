884b0912984cb4e00cd1418beba619f1f2fa883a
tkhashi <pupodenews@gmail.com>
2024-09-23 21:27:52 +0900

    webview sample first commit

---
+++ WebViewSample/WebViewSample/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace WebViewSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ WebViewSample/WebViewSample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace WebViewSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
