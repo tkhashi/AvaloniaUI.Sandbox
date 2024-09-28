1d7a212446a77e69afb7cd2991ed5a30e5cecf88
tkhashi <pupodenews@gmail.com>
2024-09-28 21:55:31 +0900

    googleキーワード検索

--- WebViewSample/WebViewSample/ViewModels/MainViewModel.cs
+++ WebViewSample/WebViewSample/ViewModels/MainViewModel.cs
@@ -4,9 +4,16 @@ namespace WebViewSample.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
-    
+    private readonly string _baseUrl = @"https://www.google.com/search?q=";
+
     [ObservableProperty]
     private string _url = "";
+    
+    [ObservableProperty]
+    private string _searchKeyword = "";
+    
+    partial void OnSearchKeywordChanged(string value)
+    {
+        Url =  $"{_baseUrl}{value}";
+    }
 }
