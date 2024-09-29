842ae315f64ddbb914fa198dca537d2deaba1d96
tkhashi <pupodenews@gmail.com>
2024-09-29 22:31:27 +0900

    Invoke Event from global key

--- GlobalHotkey/GlobalHotkey/App.axaml.cs
+++ GlobalHotkey/GlobalHotkey/App.axaml.cs
@@ -1,14 +1,3 @@
-using System;
-using System.IO;
-using System.Reactive.Concurrency;
-using System.Reactive.Linq;
-using SharpHook;
-using SharpHook.Logging;
-using SharpHook.Native;
-using SharpHook.Providers;
-using SharpHook.Reactive;
-using SharpHook.Reactive.Logging;
-
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
@@ -30,8 +19,6 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Line below is needed to remove Avalonia data validation.
-            // Without this line you will get duplicate validations from both Avalonia and CT
             BindingPlugins.DataValidators.RemoveAt(0);
             desktop.MainWindow = new MainWindow
             {
@@ -47,76 +34,5 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
-
-        
-        Directory.SetCurrentDirectory(AppContext.BaseDirectory);
-        Console.WriteLine("---------- SharpHook Sample ----------\n");
-
-        using var logSource = LogSource.RegisterOrGet(minLevel: LogLevel.Debug);
-        using var reactiveLogSource = new ReactiveLogSourceAdapter(logSource, TaskPoolScheduler.Default);
-        reactiveLogSource.MessageLogged.Subscribe(OnMessageLogged);
-
-        var provider = UioHookProvider.Instance;
-
-        Console.WriteLine("System info:");
-        Console.WriteLine($"Auto-repeat rate: {provider.GetAutoRepeatRate()}");
-        Console.WriteLine($"Auto-repeat delay: {provider.GetAutoRepeatDelay()}");
-        Console.WriteLine($"Pointer acceleration multiplier: {provider.GetPointerAccelerationMultiplier()}");
-        Console.WriteLine($"Pointer acceleration threshold: {provider.GetPointerAccelerationThreshold()}");
-        Console.WriteLine($"Pointer sensitivity: {provider.GetPointerSensitivity()}");
-        Console.WriteLine($"Multi-click time: {provider.GetMultiClickTime()}");
-
-        var screens = provider.CreateScreenInfo();
-
-        foreach (var screen in screens)
-        {
-            Console.WriteLine($"Screen #{screen.Number}: {screen.Width}x{screen.Height}; ({screen.X}, {screen.Y})");
-        }
-
-        Console.WriteLine();
-
-        logSource.MinLevel = LogLevel.Info;
-
-        Console.WriteLine("---------- Press q to quit ----------\n");
-
-        var hook = new SimpleReactiveGlobalHook(TaskPoolScheduler.Default);
-
-        hook.HookEnabled.Subscribe(OnHookEvent);
-        hook.HookDisabled.Subscribe(OnHookEvent);
-
-        hook.KeyTyped.Subscribe(OnHookEvent);
-        hook.KeyPressed.Subscribe(OnHookEvent);
-        hook.KeyReleased.Subscribe(OnHookEvent);
-        hook.KeyReleased.Subscribe(e => OnKeyReleased(e, hook));
-
-        hook.MouseClicked.Subscribe(OnHookEvent);
-        hook.MousePressed.Subscribe(OnHookEvent);
-        hook.MouseReleased.Subscribe(OnHookEvent);
-
-        hook.MouseMoved
-            .Throttle(TimeSpan.FromSeconds(1))
-            .Subscribe(OnHookEvent);
-
-        hook.MouseDragged
-            .Throttle(TimeSpan.FromSeconds(1))
-            .Subscribe(OnHookEvent);
-
-        hook.MouseWheel.Subscribe(OnHookEvent);
-
-        hook.Run();
     }
-
-    static void OnHookEvent(HookEventArgs e) =>
-        Console.WriteLine($"{e.EventTime.ToLocalTime()}: {e.RawEvent}");
-
-    static void OnMessageLogged(LogEntry logEntry) =>
-        Console.WriteLine($"{Enum.GetName(logEntry.Level)?.ToUpper()}: {logEntry.FullText}");
-
-    static void OnKeyReleased(KeyboardHookEventArgs e, IReactiveGlobalHook hook)
-    {
-        if (e.Data.KeyCode == KeyCode.VcQ)
-        {
-            hook.Dispose();
-        }
-    }       
 }
--- GlobalHotkey/GlobalHotkey/Views/MainWindow.axaml.cs
+++ GlobalHotkey/GlobalHotkey/Views/MainWindow.axaml.cs
@@ -1,5 +1,16 @@
 using Avalonia.Controls;
 
+using System;
+using System.IO;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using SharpHook;
+using SharpHook.Logging;
+using SharpHook.Native;
+using SharpHook.Providers;
+using SharpHook.Reactive;
+using SharpHook.Reactive.Logging;
+
 namespace GlobalHotkey.Views;
 
 public partial class MainWindow : Window
@@ -7,5 +18,45 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        var hook = new SimpleReactiveGlobalHook(TaskPoolScheduler.Default);
+        hook.KeyPressed.Subscribe(OnHookEvent);
+        hook.KeyReleased.Subscribe(e => OnKeyReleased(e, hook));
+        hook.Run();
+    }
+
+    private bool _isPressingControl = false;
+
+    private bool _isPressingSpace = false;
+
+    void OnHookEvent(KeyboardHookEventArgs e)
+    {
+        switch (e.Data.KeyCode)
+        {
+            case KeyCode.VcLeftControl:
+                _isPressingControl = true;
+                break;
+            case KeyCode.VcSpace:
+                _isPressingSpace = true;
+                break;
+            default:
+                break;
+        }
+        
+        if(_isPressingControl && _isPressingSpace)
+        {
+            Console.WriteLine("hgoe");
+        }
+    }
+
+    void OnKeyReleased(KeyboardHookEventArgs e, IReactiveGlobalHook hook)
+    {
+        _isPressingControl = false;
+        _isPressingSpace = false;
+
+        if (e.Data.KeyCode == KeyCode.VcQ)
+        {
+            hook.Dispose();
+        }
     }
 }
